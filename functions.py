d8c3f8b7719d669712c66fee7a8ec6446d2cfd22
Ivan Kirsanov <van.kirsanoff@yandex.ru>
2022-11-21 15:26:01 +0300

    add message caption find function

--- functions.py
+++ functions.py
@@ -13,7 +13,7 @@ def search_chat_id(msg_text: str, dep_id: dict) -> int:
     return 0
 
 
-def create_media_group(message: types.Message, album: List[types.Message]) -> types.MediaGroup:
+def create_media_group(album: List[types.Message], caption: str) -> types.MediaGroup:
     media_group = types.MediaGroup()
     for obj in album:
         if obj.photo:
@@ -22,5 +22,12 @@ def create_media_group(message: types.Message, album: List[types.Message]) -> ty
             file_id = obj[obj.content_type].file_id
 
         media_group.attach({"media": file_id, "type": obj.content_type,
-                            "caption": message.caption if album.index(obj) == 0 else ""})
+                            "caption": caption if album.index(obj) == 0 else ""})
     return media_group
+
+
+def find_caption(album: List[types.Message]) -> str:
+    for media in album:
+        if media.caption:
+            return media.caption
+    return ""
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from middleware import AlbumMiddleware
 from config import TOKEN, dep_chat_id
-from functions import create_media_group, search_chat_id
+from functions import create_media_group, search_chat_id, find_caption
 from filter import IsSourceChat
 
 bot = Bot(token=TOKEN)
@@ -15,15 +15,16 @@ logging.basicConfig(filename='app.log', format='%(asctime)s - %(message)s', leve
 @dp.message_handler(IsSourceChat(), content_types=[types.ContentType.PHOTO, types.ContentType.VIDEO,
                                                    types.ContentType.DOCUMENT])
 async def handle_albums(message: types.Message, album: List[types.Message]):
-    msg_text = album[0].caption
+    msg_text = find_caption(album)
     if not msg_text:
         await message.reply('Не отправлено, отправьте сообщение еще раз (отсутствует текст сообщения) ❌')
         return True
     chat_id = search_chat_id(msg_text, dep_chat_id)
     if not chat_id:
-        await message.reply('Не отправлено, отправьте сообщение еще раз (департамент не указан, либо написан некорректно) ❌')
-        return 0
-    media_group = create_media_group(message, album)
+        await message.reply(
+            'Не отправлено, отправьте сообщение еще раз (департамент не указан, либо написан некорректно) ❌')
+        return True
+    media_group = create_media_group(album, caption=msg_text)
     await bot.send_media_group(chat_id, media_group)
     await message.reply('Отправлено ✅')
 
@@ -31,4 +32,4 @@ async def handle_albums(message: types.Message, album: List[types.Message]):
 if __name__ == '__main__':
     dp.middleware.setup(AlbumMiddleware())
     dp.bind_filter(IsSourceChat)
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=False)
